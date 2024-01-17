ecec53e0cb16e45e1d775bd1dab3f76663e44df2
victorshli <victorshli@tencent.com>
2024-01-17 17:07:03 +0800

    更新下

--- test/100分/api负载集群统计.py
+++ test/100分/api负载集群统计.py
@@ -9,16 +9,37 @@ def  add(*args):
     a=args
     return
 #拆字符
-def chai(str):
-    list_s=str.split("/")
-    return list_s
+# def chai(str):
+#     list_s=str.split("/")
+#     return list_s
 
+#按照/拆分字符串，去掉无用元素
 for x in range(num):
+
     a=input()
-    list1.append(a)
+    b=a.split("/")
+    b.pop(0)
+    c=b
+    list1.append(c)
+
     # print(chai(list1[x]))
     # dic.update({add(list1(a)):()})
-    list_s
+
+print(list1)
+#把拆分后的数组加到字典里,字典格式为  路径：（层数，次数）
+# list1_num=len(list1)
+# for x in range(list1_num):
+#     dic.update(chai(list1[x]))
+
+for x in range(num):
+    dep = 0
+    # depstr=string(dep)
+    # depnum=dep+1
+    # if dic.keys([list1[dep]+""+dep]):
+    print(list1(dep))
+    # dic[list1[dep]+""+str(dep)]=dep
+
+
 
 
 
