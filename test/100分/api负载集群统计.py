b3ef87331f9ecbed2ca6bd23d5d889fbd72ff674
victorshli <victorshli@tencent.com>
2024-01-18 17:02:24 +0800

    词典的存储还有点问题

--- test/100分/api负载集群统计.py
+++ test/100分/api负载集群统计.py
@@ -5,13 +5,8 @@ x=0
 dic = {}
 list_s={}
 
-def  add(*args):
-    a=args
-    return
-#拆字符
-# def chai(str):
-#     list_s=str.split("/")
-#     return list_s
+dic1 ={"asd":1}
+
 
 #按照/拆分字符串，去掉无用元素
 for x in range(num):
@@ -21,30 +16,33 @@ for x in range(num):
     b.pop(0)
     c=b
     list1.append(c)
-
-    # print(chai(list1[x]))
-    # dic.update({add(list1(a)):()})
-
-print(list1)
-#把拆分后的数组加到字典里,字典格式为  路径：（层数，次数）
-# list1_num=len(list1)
-# for x in range(list1_num):
-#     dic.update(chai(list1[x]))
-
-for x in range(num):
     dep = 0
-    # depstr=string(dep)
-    # depnum=dep+1
-    # if dic.keys([list1[dep]+""+dep]):
-    print(list1(dep))
-    # dic[list1[dep]+""+str(dep)]=dep
-
-
-
-
-
-
+    for y in range(len(list1)):
+        k=list1[x][y]+str(dep)
+        if k in dic:
+            dic[k]=dic[k]+1
+        else:
+            dic[k]=0
+        dep=dep+1
+# print(list1)
 print(dic)
+answer=input()
+
+list_a=answer.split(" ")
+
+print(list_a)
+
+a1=int(list_a[0])-1
+a2=list_a[1]+str(a1)
+if a2 in  dic:
+
+    print(dic.get(a2))
+else:
+    print(0)
+
+
+
+
 
 
 
